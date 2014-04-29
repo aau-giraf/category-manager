4d90e2057deb78b23a42fb8c158a8f45a1217416
Emil Jensen <esje11@student.aau.dk>
2014-04-29 14:49:07 +0200

    Long click on categories is now possible, but does not save.

--- src/dk/aau/cs/giraf/pictoadmin/SettingDialogFragment.java
+++ src/dk/aau/cs/giraf/pictoadmin/SettingDialogFragment.java
@@ -39,49 +39,47 @@ public class SettingDialogFragment extends DialogFragment{
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         // Use the Builder class for convenient dialog construction
-//        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-//        builder.setTitle(R.string.edit_category)
-//                .setItems(R.array.dialog_options, new OnClickListener() {
-//
-//                    @Override
-//                    public void onClick(DialogInterface dialog, int which) {
-//                        //Change title
-//                        if(which == 0) {
-//                            TitleDialogFragment titelDialog = new TitleDialogFragment(startActivity, pos, isCategory);
-//                            titelDialog.show(getFragmentManager(), "changeTitle");
-//                        }
-//                        //Change color
-//                        if(which == 1) {
-//                            AmbilWarnaDialog colorDialog = new AmbilWarnaDialog(startActivity, category.getColour(), new OnAmbilWarnaListener() {
-//                                @Override
-//                                public void onOk(AmbilWarnaDialog dialog, String color) {
-//                                    category.setColour(color);
-//
-//                                    startActivity.updateSettings(category, pos, isCategory, MainActivity.Setting.COLOR);
-//                                }
-//
-//                                @Override
-//                                public void onCancel(AmbilWarnaDialog dialog) {
-//                                    // Do nothing
-//                                }
-//                            });
-//                            colorDialog.show();
-//                        }
-//                        //Change icon
-//                        if(which == 2) {
-//                            IconDialogFragment iconDialog = new IconDialogFragment(startActivity, category, pos, isCategory);
-//                            iconDialog.show(getFragmentManager(), "changeIcon");
-//                        }
-//                    }
-//                })
-//                .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
-//                    public void onClick(DialogInterface dialog, int id) {
-//                        // User cancelled the dialog
-//                    }
-//                });
-//        // Create the AlertDialog object and return it
-//        return builder.create();
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle(R.string.edit_category)
+                .setItems(R.array.dialog_options, new OnClickListener() {
 
-        return null;
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        //Change title
+                        if(which == 0) {
+                            TitleDialogFragment titelDialog = new TitleDialogFragment(startActivity, pos, isCategory);
+                            titelDialog.show(getFragmentManager(), "changeTitle");
+                        }
+                        //Change color
+                        if(which == 1) {
+                            AmbilWarnaDialog colorDialog = new AmbilWarnaDialog(startActivity, category.getColour(), new OnAmbilWarnaListener() {
+                                @Override
+                                public void onOk(AmbilWarnaDialog dialog, int color) {
+                                    category.setColour(color);
+
+                                    startActivity.updateSettings(category, pos, isCategory, MainActivity.Setting.COLOR);
+                                }
+
+                                @Override
+                                public void onCancel(AmbilWarnaDialog dialog) {
+                                    // Do nothing
+                                }
+                            });
+                            colorDialog.show();
+                        }
+                        //Change icon
+                        if(which == 2) {
+                            IconDialogFragment iconDialog = new IconDialogFragment(startActivity, category, pos, isCategory);
+                            iconDialog.show(getFragmentManager(), "changeIcon");
+                        }
+                    }
+                })
+                .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // User cancelled the dialog
+                    }
+                });
+        // Create the AlertDialog object and return it
+        return builder.create();
     }
 }
