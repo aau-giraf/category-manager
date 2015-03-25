827ed8150f099012172a58ed6feb784429bb4261
Niklas Kirk Mouritzsen <nkmouritzsen@gmail.com>
2015-03-25 14:41:05 +0100

    Add new default activity

---
+++ src/main/java/dk/aau/cs/giraf/cat/CategoryActivity.java
@@ -0,0 +1,193 @@
+package dk.aau.cs.giraf.cat;
+
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import java.util.List;
+
+import dk.aau.cs.giraf.activity.GirafActivity;
+import dk.aau.cs.giraf.cat.fragments.InitialFragment;
+import dk.aau.cs.giraf.oasis.lib.Helper;
+import dk.aau.cs.giraf.oasis.lib.models.Category;
+import dk.aau.cs.giraf.oasis.lib.models.Profile;
+
+
+public class CategoryActivity extends GirafActivity implements InitialFragment.OnFragmentInteractionListener {
+
+    // Identifiers used to start activities for results
+    public final int CREATE_CATEGORY_REQUEST = 1;
+
+    // Helper that will be used to fetch profiles
+    private final Helper helper = new Helper(this);
+
+    // Profiles of which the categories will be loaded from
+    private Profile childProfile;
+    private Profile guardianProfile;
+
+    // View to contain categories
+    private ListView categoryContainer;
+
+    private class LoadCategoriesTask extends AsyncTask<Void, Void, List<Category>> {
+
+        @Override
+        protected List<Category> doInBackground(Void... params) {
+            return helper.categoryHelper.getCategoriesByProfileId(getCurrentUser().getId());
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+            // Set view when list is empty
+            categoryContainer.setEmptyView(new ProgressBar(CategoryActivity.this));
+
+            // TODO: Remove this
+            Toast.makeText(CategoryActivity.this, "Den er begyndt!", Toast.LENGTH_LONG).show();
+        }
+
+        protected void onPostExecute(List<Category> result) {
+            PictogramCategoryAdapter categoryAdapter = new PictogramCategoryAdapter(result, CategoryActivity.this);
+            categoryContainer.setAdapter(categoryAdapter);
+
+            // Set view when list is empty
+            categoryContainer.setEmptyView(findViewById(R.id.empty_list_item));
+
+            // TODO: Remove this
+            Toast.makeText(CategoryActivity.this, "Den er færdig!", Toast.LENGTH_LONG).show();
+        }
+
+    };
+
+    /**
+     * Will return the current profile. If the application is launched from a child profile,
+     * this method will return that profile. If the application is launched from a guardian profile,
+     * this method will return that profile instead.
+     * <p/>
+     * Notice that when the application is launched from a child profile, the guardian profile is
+     * also assigned.
+     *
+     * @return the current profile. {@code null} if no current profile.
+     */
+    private Profile getCurrentUser() {
+        if (childProfile != null) {
+            return childProfile;
+        } else if (guardianProfile != null) {
+            return guardianProfile;
+        }
+
+        return null;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_category);
+
+        // Set the content of the frame layout to the default fragment
+        setContent(InitialFragment.newInstance(), R.id.categorytool_framelayout);
+
+        // Get the extra information from when the activity was started (contains profile ids etc.)
+        Bundle extras = getIntent().getExtras();
+
+        // Test if the activity was started correctly
+        if (extras == null) {
+            // TODO: Handle it. The activity was started incorrectly
+        } else {
+            int childId = extras.getInt("currentChildID");
+            int guardianId = extras.getInt("currentGuardianID");
+
+            if (childId != -1) {
+                childProfile = helper.profilesHelper.getProfileById(childId);
+            }
+
+            if (guardianId != -1) {
+                guardianProfile = helper.profilesHelper.getProfileById(guardianId);
+            }
+        }
+
+        // Find the ListView that will contain the categories
+        categoryContainer = (ListView) this.findViewById(R.id.category_container);
+
+        // Load the categories using the LoadCategoriesTask
+        LoadCategoriesTask categoryLoader = (LoadCategoriesTask) new LoadCategoriesTask().execute();
+    }
+
+    /**
+     * Will set the content (fragment) of the provided {@code FrameLayout}
+     *
+     * @param fragment            fragment to place in specific {@code FrameLayout}, see {@code frameLayoutResource}
+     * @param frameLayoutResource resource to place fragment in
+     */
+    public void setContent(final Fragment fragment, final int frameLayoutResource) {
+        final FragmentManager fm = getSupportFragmentManager();
+
+        fm.popBackStackImmediate(null, FragmentManager.POP_BACK_STACK_INCLUSIVE);
+        fm.popBackStackImmediate();
+
+        final Fragment currentContent = getSupportFragmentManager().findFragmentById(frameLayoutResource);
+
+        if (currentContent != null && !fragment.getClass().equals(InitialFragment.class)) {
+            fm.beginTransaction().replace(frameLayoutResource, InitialFragment.newInstance()).commit();
+            fm.executePendingTransactions();
+            fm.beginTransaction().replace(frameLayoutResource, fragment).addToBackStack(null).commit();
+        } else {
+            fm.beginTransaction().replace(frameLayoutResource, fragment).commit();
+        }
+    }
+
+    /**
+     * Push the provided (fragment) on top of the current fragment stack, see {@code setContent}
+     *
+     * @param fragment            fragment to place in specific {@code FrameLayout}, see {@code frameLayoutResource}
+     * @param frameLayoutResource resource to place fragment in
+     */
+    public void pushContent(final Fragment fragment, final int frameLayoutResource) {
+        final FragmentManager fm = getSupportFragmentManager();
+
+        final Fragment currentContent = getSupportFragmentManager().findFragmentById(frameLayoutResource);
+
+        if (currentContent == fragment) {
+            fm.beginTransaction().remove(currentContent).commit();
+            fm.executePendingTransactions();
+        }
+
+        fm.beginTransaction().replace(frameLayoutResource, fragment).addToBackStack(null).commit();
+    }
+
+    /**
+     * Will be called when the "create category" button is pressed
+     */
+    @Override
+    public void onCreateCategoryButtonClicked(View view) {
+        // Check if the current profile is a guardian profile
+        if (guardianProfile == null) {
+            // TODO: Handle it. Crash app or open select user dialog
+        } else {
+            Intent intent = new Intent(this, CreateCategoryActivity.class);
+            intent.putExtra("currentGuardianID", guardianProfile.getId());
+            startActivityForResult(intent, CREATE_CATEGORY_REQUEST);
+        }
+    }
+
+    /**
+     * Will be called when an opened activity returns
+     */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // Check which request we're responding to
+        if (requestCode == CREATE_CATEGORY_REQUEST) {
+            // Make sure the request was successful
+            if (resultCode == RESULT_OK) {
+                // Reload all categories for the current profile
+                LoadCategoriesTask categoryLoader = (LoadCategoriesTask) new LoadCategoriesTask().execute();
+            }
+        }
+    }
+}
---
+++ src/main/java/dk/aau/cs/giraf/cat/fragments/InitialFragment.java
@@ -0,0 +1,87 @@
+package dk.aau.cs.giraf.cat.fragments;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import dk.aau.cs.giraf.cat.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link InitialFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link InitialFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class InitialFragment extends Fragment {
+
+    private OnFragmentInteractionListener mListener;
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @return A new instance of fragment InitialFragment.
+     */
+    public static InitialFragment newInstance() {
+        InitialFragment fragment = new InitialFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    public InitialFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (getArguments() != null) {
+            // Add arguments here if needed. Do it this way:
+            // mParam1 = getArguments().getString(ARG_PARAM1);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_initial, container, false);
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        try {
+            mListener = (OnFragmentInteractionListener) activity;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(activity.toString() + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p/>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        public void onCreateCategoryButtonClicked(View view);
+    }
+
+}
